7033c22c3c26ca6a148d10ebf8bc86c0a247ce41
root <root@ip-172-31-21-65.ec2.internal>
2025-11-04 17:25:10 +0000

    python code added to my.py file

---
+++ my.py
@@ -0,0 +1,10 @@
+code 1 of python im adding here
+
+# Input from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+# Adding two numbers
+sum = num1 + num2
+# Display the sum
+print("The sum is:", sum)
+
